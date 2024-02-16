076cd8ff64019e47b3cbc7a09d27f747f599c2aa
Dragos Stan <dragos.stan2602@gmail.com>
2024-02-16 14:42:17 +0100

    Added output handling in python script

--- src/script.py
+++ src/script.py
@@ -5,5 +5,5 @@ tf_apply_results = json.loads(os.getenv('TF_APPLY_RESULTS'))
 
 print(tf_apply_results)
 
-# for k, v in tf_apply_results:
-#     print(k)
+for k, v in tf_apply_results.items():
+    print(k)
